a1badae34980d72bf75536590bec3329c1b0ed1f
Harold Carr <harold.carr@gmail.com>
2001-03-02 20:10:51 +0000

    Checkpoint before further work.

--- org/llava/impl/runtime/NamespaceImpl.java
+++ org/llava/impl/runtime/NamespaceImpl.java
@@ -1,5 +1,5 @@
 //
-// Created       : 2000 Oct 23 (Mon) 17:35:17 by .
+// Created       : 2000 Oct 23 (Mon) 17:35:17 by Harold Carr.
 // Last Modified : 2004 Dec 01 (Wed) 10:15:34 by Harold Carr.
 //
 
@@ -13,19 +13,57 @@ public class Namespace
     private String name;
     private Hashtable map;
     private Vector refList;
+    private long fileLastModified; // Non-zero if associated file.
+    private boolean classAlreadyImported; // true if associated class.
 
-    public Namespace (String name, Namespace topLevel)
+    public Namespace (String name, Namespace root)
     {
-	this.name    = name;
-	this.map     = new Hashtable();
-	this.refList = new Vector();
+	this.name             = name;
+	this.map              = new Hashtable();
+	this.refList          = new Vector();
+	this.fileLastModified = 0;
+	this.classAlreadyImported = false;
+
 	refList.add(0, this); // Self at front.
-	refList.add(topLevel); // topLevel always at end.
+	refList.add(root);    // root always at end.
     }
 
-    public String getName() { return name; }
-    public Hashtable getMap() { return map; }
-    public Vector getRefList() { return refList; }
+    public String getName () 
+    {
+	return name; 
+    }
+
+    public Hashtable getMap ()
+    {
+	return map; 
+    }
+    
+    public Vector getRefList ()
+    {
+	return refList; 
+    }
+
+    public long setFileLastModified (long lastModified)
+    {
+	fileLastModified = lastModified;
+	return fileLastModified;
+    }
+
+    public long getFileLastModified ()
+    {
+	return fileLastModified; 
+    }
+
+    public boolean setClassAlreadyImported (boolean b)
+    {
+	classAlreadyImported = b; 
+	return classAlreadyImported;
+    }
+
+    public boolean classAlreadyImported ()
+    {
+	return classAlreadyImported; 
+    }
 }
 
 // End of file.
