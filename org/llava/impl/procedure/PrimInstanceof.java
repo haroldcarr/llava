638b39c5502461b651552dd30968bdac6ac17115
Harold Carr <harold.carr@gmail.com>
2004-11-30 05:14:56 +0000

    works with import now

--- org/llava/impl/procedure/PrimInstanceof.java
+++ org/llava/impl/procedure/PrimInstanceof.java
@@ -12,7 +12,7 @@ or send a letter to
 
 /**
  * Created       : 2000 Jan 18 (Tue) 04:24:10 by Harold Carr.
- * Last Modified : 2004 Sep 03 (Fri) 15:34:50 by Harold Carr.
+ * Last Modified : 2004 Nov 29 (Mon) 20:39:00 by Harold Carr.
  */
 
 package org.llava.impl.runtime.procedure.primitive.java.opt;
@@ -21,26 +21,37 @@ import org.llava.impl.F;
 import org.llava.lang.types.Pair;
 import org.llava.impl.runtime.Engine;
 import org.llava.impl.runtime.FR;
+import org.llava.impl.runtime.env.Namespace;
 import org.llava.impl.runtime.procedure.primitive.PrimitiveProcedure;
+
 
 public class PrimInstanceof
     extends
 	PrimitiveProcedure
 {
+    private Namespace namespace;
+
     public PrimInstanceof ()
     {
     }
 
-    public PrimInstanceof newPrimInstanceof ()
+    public PrimInstanceof (Namespace namespace)
     {
-	return new PrimInstanceof();
+	this.namespace = namespace;
+    }
+
+    public PrimInstanceof newPrimInstanceof (Namespace namespace)
+    {
+	return new PrimInstanceof(namespace);
     }
 
     public Object apply (Pair args, Engine engine)
     {
 	checkNumArgs(args, 2);
+	String className = args.second().toString();
+	className = namespace.getFullNameForClass(className);
 	try {
-	    return F.newBoolean(Class.forName(args.second().toString())
+	    return F.newBoolean(Class.forName(className)
 				.isInstance(args.first()));
 	} catch (Throwable e) {
 	    throw F.newLlavaException(e);
