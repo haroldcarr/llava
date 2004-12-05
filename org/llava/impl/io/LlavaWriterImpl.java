5d4943f14271a381722ca88653c901d0f2d0b18a
Harold Carr <harold.carr@gmail.com>
2004-12-05 17:35:14 +0000

    Added WRITE - LlavaWriter(Impl).  Wrote in Java from prototype in Llava.

--- org/llava/impl/io/LlavaWriterImpl.java
+++ org/llava/impl/io/LlavaWriterImpl.java
@@ -1,82 +1,131 @@
-;;;;
-;;;; Copyright (c) 1997 - 2004 Harold Carr
-;;;;
-;;;; This work is licensed under the Creative Commons Attribution License.
-;;;; To view a copy of this license, visit 
-;;;;  http://creativecommons.org/licenses/by/2.0/
-;;;; or send a letter to
-;;;;  Creative Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
-;;;; -------------------------------------------------------------------------
+/*
+Copyright (c) 1997 - 2004 Harold Carr
 
-;;;;
-;;;; Created       : 2004 Dec 04 (Sat) 08:47:29 by Harold Carr.
-;;;; Last Modified : 2004 Dec 08 (Wed) 08:59:54 by Harold Carr.
-;;;;
+This work is licensed under the Creative Commons Attribution License.
+To view a copy of this license, visit 
+  http://creativecommons.org/licenses/by/2.0/
+or send a letter to
+  Creative Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
+------------------------------------------------------------------------------
+*/
 
-(package org.openhc.write)
+/**
+ * Created       : 1999 Dec 17 (Fri) 20:11:43 by Harold Carr.
+ * Last Modified : 2004 Dec 08 (Wed) 08:59:54 by Harold Carr.
+ */
 
-(define (write xx)
-  (cond ((null? xx)
-	 (display xx))
+package org.llava.impl.io;
 
-	((instanceof xx 'java.lang.String)
-	 (display "\"")
-	 (display xx)
-	 (display "\""))
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
-	((instanceof xx 'org.llava.lang.types.Pair)
-	 (write-pair xx))
+import org.llava.impl.F;
+import org.llava.io.LlavaWriter;
+import org.llava.lang.types.Pair;
+import org.llava.lang.types.Symbol;
+import org.llava.impl.util.List;
 
-	((instanceof xx 'java.lang.Character)
-	 (display "#\\")
-	 (display xx))
+public class LlavaWriterImpl
+    implements
+	LlavaWriter
+{
+    private Symbol QUOTE            = F.newSymbol("quote");
+    private Symbol QUASIQUOTE       = F.newSymbol("quasiquote");
+    private Symbol UNQUOTE          = F.newSymbol("unquote");
+    private Symbol UNQUOTE_SPLICING = F.newSymbol("unquote-splicing");
 
-	((isArray (getClass xx))
-	 (display "#")
-	 (write (vector->list xx)))
+    private PrintWriter out;
 
-	(else
-	 (display xx)))
-  xx)
+    public LlavaWriterImpl ()
+    {
+    }
 
-(define (write-pair pair)
-  (cond ((and (not (null? (cdr pair)))
-	      (memq (car pair) 
-		    (list 'quote 'quasiquote 'unquote 'unquote-splicing)))
-	 (display
-	  (case (car pair)
-	    ((quote)            "'")
-	    ((quasiquote)       "`")
-	    ((unquote)          ",")
-	    ((unquote-splicing) ",@"))
-	 )
-	 (write (cadr pair)))
-	(else
-	 (display "(")
-	 (write (car pair))
-	 (write-list (cdr pair)))))
+    LlavaWriterImpl (PrintWriter out)
+    {
+	this.out = out;
+    }
 
-(define (write-list list)
-  (cond ((null? list)
-	 (display ")"))
-	((not (pair? list))
-	 (display " . ")
-	 (write list)
-	 (display ")"))
-	(else
-	 (display " ")
-	 (write (car list))
-	 (write-list (cdr list)))))
+    public LlavaWriter newLlavaWriter ()
+    {
+	return new LlavaWriterImpl();
+    }
 
-(-comment-
-(import org.openhc.write)
+    public LlavaWriter newLlavaWriter (PrintWriter out)
+    {
+	return new LlavaWriterImpl(out);
+    }
 
-(begin
-(write (list 'a 3 3.4 null true false "foo" '(b . c)  #((1 . 2) #(3 4)) #\c
-	     (new 'java.util.Hashtable)
-	     '`(a ,b ,@(3 4))))
-null)
-)
-	  
-;;; End of file.
+    public Object write (Object x)
+    {
+	if (x == null) {
+	    emit(null);
+	} else if (x instanceof java.lang.String) {
+	    emit( "\"");
+	    emit(x);
+	    emit("\"");
+	} else if (x instanceof Pair) {
+	    writePair((Pair)x);
+	} else if (x instanceof java.lang.Character) {
+	    emit("#\\");
+	    emit(x);
+	} else if (x.getClass().isArray()) {
+	    emit("#");
+	    write(List.vector2list(x));
+	} else {
+	    emit(x);
+	}
+	return x;
+    }
+
+    public PrintWriter getPrintWriter ()
+    {
+	return out;
+    }
+
+    private Object writePair(Pair pair) 
+    {
+	String uq = null;
+
+	if (pair.cdr() != null) {
+	    Object x = pair.car();
+	    if      (x == QUOTE)            uq = "'";
+	    else if (x == QUASIQUOTE)       uq = "`";
+	    else if (x == UNQUOTE)          uq = ",";
+	    else if (x == UNQUOTE_SPLICING) uq = ",@";
+	}
+
+	if (uq != null) {
+	    emit(uq);
+	    write(pair.cadr());
+	} else {
+	    emit("(");
+	    write(pair.car());
+	    writeList(pair.cdr());
+	}
+	return pair;
+    }
+
+    private void writeList(Object list)
+    {
+	if (list == null) {
+	    emit(")");
+	} else if (! (list instanceof Pair)) {
+	    emit(" . ");
+	    write(list);
+	    emit(")");
+	} else {
+	    emit(" ");
+	    write(((Pair)list).car());
+	    writeList(((Pair)list).cdr());
+	}
+    }
+
+    private void emit(Object x)
+    {
+	out.print(x);
+    }
+}
+
+// End of file.
 
