9f288884f1f712cb8ec33e0d46b7c8ca5d531f92
Harold Carr <harold.carr@gmail.com>
2005-03-13 00:55:12 +0000

    Initial revision

---
+++ org/llava/Syntax.java
@@ -0,0 +1,42 @@
+/*
+Copyright (c) 1997 - 2004 Harold Carr
+
+This work is licensed under the Creative Commons Attribution License.
+To view a copy of this license, visit 
+  http://creativecommons.org/licenses/by/2.0/
+or send a letter to
+  Creative Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
+------------------------------------------------------------------------------
+*/
+
+/**
+ * Created       : @author Peter Norvig, Copyright 1998, http://www.norvig.com/license.html 
+ * Last Modified : 2005 Mar 12 (Sat) 17:34:17 by Harold Carr.
+ */
+
+package org.llava;
+
+import org.llava.Pair;
+import org.llava.compiler.Compiler;
+import org.llava.compiler.EnvironmentLexical;
+import org.llava.runtime.LlavaRuntime;
+import org.llava.runtime.Engine;
+
+import org.llava.impl.runtime.Code;
+
+public interface Syntax
+{
+    public Code compile (Compiler           compiler,
+		         Pair               x, 
+			 EnvironmentLexical e, 
+			 LlavaRuntime       runtime);
+
+    public Object apply (Pair args, Engine engine);
+
+    public String getName ();
+
+    public String setName (String name);
+}
+
+// End of file.
+
